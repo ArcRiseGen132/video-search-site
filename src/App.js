10f2dcb7857c0b35f87b8287b933c7f2bf257d91
Sheikh_Ahmed <ssahme21@gmail.com>
2022-10-14 12:18:31 -0400

    Created Searchbar, installed packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import youtube from "./API/youtube";
+import { Grid } from "@mui/material";
+import SearchBar from "./Components/SearchBar/SearchBar";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit}></SearchBar>
+          </Grid>
+          <Grid item xs={8}>
+            {/* {VideoDetail} */}
+          </Grid>
+          <Grid item xs={4}>
+            {/* {VideoList} */}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
+
+  async function handleSubmit(searchItem) {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 10,
+        key: API_KEY,
+        q: searchItem,
+      },
+    });
+    console.log(response.data.items);
+  }
 }
 
 export default App;
